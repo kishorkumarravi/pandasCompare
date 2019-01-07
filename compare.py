5bd13c3d836ff7376ece8f3ec7a0c6a32c0f46b5
Kishor <kishorkumarravi@gmail.com>
2019-01-07 22:50:45 +0530

    Compared based on shape

--- compare.py
+++ compare.py
@@ -6,7 +6,11 @@ print("hi...")
 df1 = pd.read_csv("data1.csv")
 df2 = pd.read_csv("data2.csv")
 
-print(df1)
-print(df2)
+df1Shape = df1.shape[0]
+df2Shape = df2.shape[0]
 
-
+if (df1Shape == df2Shape):
+    df = pd.concat([df1, df2, df2]).drop_duplicates(keep=False)
+    print("df ", df)
+else:
+    print("different dataframe")
